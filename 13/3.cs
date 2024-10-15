c3052df0a2d4f223dde97b704bff385540500127
Quico Saval <quicosv@gmail.com>
2024-10-15 18:04:09 +0200

    Añade el capítulo 13

---
+++ 13/3.cs
@@ -0,0 +1,7 @@
+﻿string j = "";
+for (int i = 0; i < 10; i++)
+{
+	j = i.ToString();
+	Console.WriteLine(i);
+}
+Console.WriteLine("Fuera del bucle: " + j);
---
+++ 13/4.cs
@@ -0,0 +1,14 @@
+﻿Coche? miCoche = new Coche();
+miCoche.HacerAlgo();
+
+class Coche
+{
+	public void HacerAlgo()
+	{
+		Console.WriteLine(MetodoAuxiliar());
+	}
+	private string MetodoAuxiliar()
+	{
+		return "Hola mudno";
+	}
+}
