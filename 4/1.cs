0ba55bb3b9f8b7e5ca88a47096d6ada3854f07a7
Quico Saval <quicosv@gmail.com>
2024-10-15 11:41:32 +0200

    Corrige el texto del capítulo 4 y pone el código en su carpeta correspondiente

---
+++ 4/1.cs
@@ -0,0 +1,39 @@
+﻿// Declaración de variables
+int x, y, a, b;
+// Operador de asignación
+x = 3;
+y = 4;
+a = 1;
+b = 0;
+// Hay bastantes operadores matemáticos...
+// suma
+x = 3 + 4;
+// Resta
+x = 4 - 3;
+// Multiplicación
+x = 10 * 5;
+// Ordenar cálculos con paréntesis
+x = (x + y) * (a - b);
+// Hay muchos operadores usados para evaluar valores...
+// Operador de igualdad
+if (x == y) { }
+// Operador mayor que
+if (x > y) { }
+// Operador menor que
+if (x < y)
+{ }
+// Operador mayor o igual
+if (x >= y) { }
+// Operador menor o igual
+if (x <= y) { }
+/* 
+Hay otros dos operadores que se pueden usar para expandir o mejorar una evaluación y se pueden combinar
+ */
+// Operador y
+if ((x > y) && (a > b)) { }
+// Operador o
+if ((x > y) || (a > b)) { }
+// Operador ternario
+string mensaje = (x == 1) ? "coche" : "barco";
+// Acceso a miembros e invocación de métodos
+Console.WriteLine("Hola");
