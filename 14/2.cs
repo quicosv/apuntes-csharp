5b4f90224d4bc81a92b0bf22fa6e99b063f3b4be
Quico Saval <quicosv@gmail.com>
2024-10-16 15:41:35 +0200

    Añade el capítulo 14

---
+++ 14/2.cs
@@ -0,0 +1,23 @@
+﻿Int64 x;
+try
+{
+	//Abre el archivo
+	StreamWriter sw = new StreamWriter("C:\\Test1.txt", true, Encoding.UTF8);
+
+	//Escribe los números del 1 al 10 en la misma línea
+	for(x=0; x < 10; x++)
+	{
+	sw.Write(x);
+	}
+
+	//Cierra el archivo
+	sw.Close();
+}
+catch(Exception e)
+{
+	Console.WriteLine("Excepción: " + e.Message);
+}
+finally
+{
+	Console.WriteLine("Ejecutando el bloque finally.");
+}
