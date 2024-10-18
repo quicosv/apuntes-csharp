b70f615c28089e44c456b7b0e45d3d43524296eb
Quico Saval <quicosv@gmail.com>
2024-10-18 13:43:59 +0200

    Empieza el capítulo de las bibliotecas de clases personalizadas

---
+++ 15/1.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace MiBiblioteca;
+
+public class Arreglo
+{
+	public string fuenteWeb(string url)
+	{
+		WebClient cliente = new WebClient();
+		return cliente.DownloadString(url);
+	}
+	public string fuenteWeb(string url, String ruta)
+	{
+		WebClient cliente = new WebClient();
+		string respuesta = cliente.DownloadString(url);
+		File.WriteAllText(ruta, respuesta);
+		return respuesta;
+	}
+}
---
+++ 15/2.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+
+namespace MiBiblioteca;
+
+public class Arreglo
+{
+	public string fuenteWeb(string url)
+	{
+		return obtenerFuente(url);
+	}
+	public string fuenteWeb(string url, String ruta)
+	{
+		string respuesta = obtenerFuente(url);
+		File.WriteAllText(ruta, respuesta);
+		return respuesta;
+	}
+	private string obtenerFuente(string url)
+	{
+		WebClient cliente = new WebClient();
+		return cliente.DownloadString(url);
+	}
+}
