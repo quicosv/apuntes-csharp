2bf65761fb1323c8f7224a721cda801270410e6d
Quico Saval <quicosv@gmail.com>
2024-10-25 17:06:05 +0200

    Añade la parte que se ha podido hacer del capítulo de los eventos

---
+++ 20/1.cs
@@ -0,0 +1,9 @@
+﻿using System.Timers;
+
+System.Timers.Timer miCronometro = new System.Timers.Timer(2000);
+miCronometro.Elapsed += TiempoTranscurrido;
+
+void TiempoTranscurrido(object? sender, ElapsedEventArgs e)
+{
+	throw new NotImplementedException();
+}
---
+++ 20/2.cs
@@ -0,0 +1,10 @@
+﻿using System.Timers;
+
+System.Timers.Timer miCronometro = new System.Timers.Timer(2000);
+miCronometro.Elapsed += TiempoTranscurrido;
+miCronometro.Start();
+Console.ReadLine();
+void TiempoTranscurrido(object? sender, ElapsedEventArgs e)
+{
+	Console.WriteLine("{0:HH:mm:ss.fff}.", e.SignalTime);
+}
